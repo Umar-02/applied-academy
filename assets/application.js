4469fe04884dfb7de5a87e2cceb38e2c4f48adce
Umar-02 <umarfront@gmail.com>
2024-05-17 23:52:30 +0500

    cart-drawer

--- assets/application.js
+++ assets/application.js
@@ -42,3 +42,103 @@ var formatMoney = function(cents, format) {
 
   return formatString.replace(placeholderRegex, value);
 };
+
+// CART DRAWER
+
+function openCartDrawer() {
+  document.querySelector(".cart-drawer").classList.remove("hidden")
+}
+
+function closeCartDrawer() {
+  document.querySelector(".cart-drawer").classList.add("hidden")
+}
+
+async function updateCart() {
+  const res = await fetch("/?section_id=cart-drawer")
+  const text = await res.text()
+  const html = document.createElement("div")
+  html.innerHTML = text
+
+  const newBox = html.querySelector('.cart-drawer').innerHTML
+
+  document.querySelector('.cart-drawer').innerHTML = newBox
+  console.log(newBox);
+  addCartDrawerListeners()
+
+}
+
+function updateCartItemCount(count) {
+  document.querySelectorAll('.cart-items-count').forEach((el) => {
+    el.textContent = count
+  })
+}
+
+function addCartDrawerListeners() {
+  document.querySelectorAll(".cart-drawer-quantity-selector button").forEach((button) => {
+    button.addEventListener("click", async () => {
+      const rootItem = button.parentElement.parentElement
+
+      const key = rootItem.getAttribute('data-line-item-key')
+
+      const currentQuantity = Number(button.parentElement.querySelector('input').value)
+      const isUp = button.classList.contains('cart-drawer-add--quantity')
+
+      const newQuantity = isUp ? currentQuantity + 1 : currentQuantity - 1
+
+
+      const res = await fetch('/cart/update.js', {
+        method: "post",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify( {updates: {[key]: newQuantity } })
+      })
+
+      const cart = await res.json()
+
+      updateCartItemCount(cart.item_count)
+
+      updateCart()
+    })
+  })
+
+  document.querySelector(".cart-drawer-box").addEventListener("click", (e) => {
+    e.stopPropagation()
+  })
+
+  document.querySelectorAll(".cart-drawer-close, .cart-drawer").forEach((el) => {
+    el.addEventListener("click", () => {
+      closeCartDrawer()
+    })
+  })
+
+}
+
+document.querySelectorAll('form[action="/cart/add"]').forEach((form) => {
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+
+    fetch("/cart/add", {
+      method: 'post',
+      body: new FormData(form)
+    })
+
+    const res = await fetch('/cart.js')
+    const cart  = await res.json()
+    updateCartItemCount(cart.item_count)
+
+    await updateCart()
+    openCartDrawer()
+  })
+});
+
+document.querySelectorAll("a[href='/cart']").forEach(a => {
+  a.addEventListener('click', (e) => {
+    e.preventDefault()
+    openCartDrawer()
+  })
+})
+
+addCartDrawerListeners()
+
